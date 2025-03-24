4f18491b9e4ac0fcc34d34ca033cd70be4f1067a
azalia17 <azaliaaps17@gmail.com>
2025-03-24 19:58:56 +0700

    Initial Commit

--- BSDLink/Views/Main/DiscoverView.swift
+++ BSDLink/Views/Main/DiscoverView.swift
@@ -15,12 +15,17 @@ struct DiscoverView: View {
     @State private var routePolylines: [MKPolyline] = []
     @State private var startingPoint: String = ""
     @State private var destinationPoint: String = ""
+    @State private var showResultRoute = false
+    @State private var route: MKRoute?
     
     
     var body: some View {
         Map(initialPosition: cameraPosition) {
-            Marker("Istiqlal", systemImage: "moon.fill", coordinate: .bbb)
-                            .tint(.green)
+            Marker("Halte A", systemImage: "bus", coordinate: .bbb)
+                .tint(.orange.gradient)
+            
+            Marker("Halte B", systemImage: "bus", coordinate: .ccc)
+                            .tint(.orange.gradient)
             
             Annotation("Istiqlal", coordinate: .istiqlal, anchor: .bottom) {
                 Image(systemName: "moon.fill")
@@ -30,20 +35,20 @@ struct DiscoverView: View {
                     .frame(width: 20, height: 20)
                     .padding(7)
                     .background(.orange.gradient, in: .circle)
-//                    .contextMenu {
-//                        Button("Get Directions", systemImage: "arrow.turn.down.right") {
-//                            
-//                        }
-//                    }
             }
             
             UserAnnotation()
+            
+            if let route {
+                MapPolyline(route)
+                    .stroke(.orange, style: StrokeStyle(lineWidth: 3, dash: [5, 2]))
+            }
             ForEach(routePolylines, id: \.self) { polyline in
                 MapPolyline(polyline)
-                    .stroke(.blue, lineWidth: 5)
+                    .stroke(.orange, lineWidth: 3)
             }
         }
-        .tint(.pink)
+        .tint(.orange)
         .onAppear {
             locationManager.requestWhenInUseAuthorization()
         }
@@ -56,91 +61,120 @@ struct DiscoverView: View {
         }
         .mapStyle(.standard(elevation: .realistic))
         .overlay(alignment: .topLeading) {
-            HStack {
-                GroupBox {
-                    HStack{
-                        VStack {
-                            Label("From", systemImage: "mappin.and.ellipse")
-                            Spacer()
-                            Label("To", systemImage: "flag.filled.and.flag.crossed")
-                        }
-                        VStack {
-                            TextField("", text: $destinationPoint)
+            VStack(alignment: .leading) {
+                HStack {
+                    GroupBox {
+                        HStack{
+                            VStack {
+                                Label("From", systemImage: "mappin.and.ellipse")
+                                    .opacity(0.5)
+                                Spacer()
+                                Label("To", systemImage: "flag.filled.and.flag.crossed")
+                                    .opacity(0.5)
+                            }
+                            VStack {
+                                TextField("", text: $startingPoint)
+                                Spacer()
+                                Divider()
+                                Spacer()
+                                TextField("", text: $destinationPoint)
+                            }
+                            
                             Spacer()
                             Divider()
                             Spacer()
-                            TextField("", text: $startingPoint)
-                                
-                        }
-                           
-                        Spacer()
-                        Divider()
-                        Spacer()
-                        
-                        Button("Search", systemImage: "rectangle.2.swap") {
                             
+                            Button("Search", systemImage: "rectangle.2.swap") {
+                                
+                            }
+                            
+                            .frame(height: 35, alignment: .center)
+                            //                    .background(.white)
+                            .labelStyle(.iconOnly)
+                            //                        .buttonStyle(.borderedProminent)
+                            
+                            //                        .tint(.white)
+                            .foregroundColor(.black)
+                            //                        .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
+                            
+                        }
+                        .frame(height: 75)
+                        //                    Toggle(isOn: "$userAgreed") {
+                        //                        Text("I agree to the above terms")
+                        //                    }
+                    }
+                    
+                    //                .groupBoxStyle(DefaultGroupBoxStyle())
+                    
+                    //            .frame(width: .)
+                    //            .foregroundColor(.white)
+                    //            .background(.white)
+                    //                .tint(.white)
+                    
+                    .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
+                    //            TextField("From", text: $startingPoint)
+                    //                .safeAreaPadding()
+                    //                .background(.white)
+                    
+                    VStack {
+                        Button("Search", systemImage: "magnifyingglass") {
+                            getWalkingDirections(to: .bbb)
+                            getDirections()
+                            
+                            //                        showResultRoute = true
                         }
                         
                         .frame(height: 35, alignment: .center)
-    //                    .background(.white)
+                        //                    .background(.white)
                         .labelStyle(.iconOnly)
-//                        .buttonStyle(.borderedProminent)
+                        .buttonStyle(.borderedProminent)
                         
-//                        .tint(.white)
+                        .tint(.white)
                         .foregroundColor(.black)
-//                        .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
+                        .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
                         
-                    }
-                        .frame(height: 75)
-    //                    Toggle(isOn: "$userAgreed") {
-    //                        Text("I agree to the above terms")
-    //                    }
-                    }
-                
-//                .groupBoxStyle(DefaultGroupBoxStyle())
-                
-    //            .frame(width: .)
-    //            .foregroundColor(.white)
-    //            .background(.white)
-//                .tint(.white)
-                
-                .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
-    //            TextField("From", text: $startingPoint)
-    //                .safeAreaPadding()
-    //                .background(.white)
-                
-                VStack {
-                    Button("Search", systemImage: "magnifyingglass") {
-                        getDirections(to: .istiqlal)
-                    }
-                    
-                    .frame(height: 35, alignment: .center)
-//                    .background(.white)
-                    .labelStyle(.iconOnly)
-                    .buttonStyle(.borderedProminent)
-                    
-                    .tint(.white)
-                    .foregroundColor(.black)
-                    .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
-                    
-                    
-                    
-                    Spacer()
-                    
-                    Button("Filter", systemImage: "slider.vertical.3") {
                         
+                        
+                        Spacer()
+                        
+                        Button("Filter", systemImage: "slider.vertical.3") {
+                            
+                        }
+                        .labelStyle(.iconOnly)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.white)
+                        .foregroundColor(.black)
+                        .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
                     }
-                    .labelStyle(.iconOnly)
-                    .buttonStyle(.borderedProminent)
-                    .tint(.white)
-                    .foregroundColor(.black)
-                    .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
                 }
+                
+                ScrollView {
+                    HStack {
+                        ForEach(1..<3) { index in
+                            Button ("Route \(index) - \(index + 1)") {
+                                startingPoint = "Route \(index)"
+                                destinationPoint = "Route \(index + 1)"
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.white)
+                            .foregroundColor(.black)
+                            .shadow(color: Color.black.opacity(0.1), radius: 15, x: 0, y: 10)
+                        }
+                    }
+                }
+//                .padding()
+//                .fram÷
+
             }
-            .frame(height: 100)
+            .frame(height: 150)
             .safeAreaPadding()
 //            .scaledToFit()
         }
+        .sheet(isPresented: $showResultRoute, content: {
+            Text("This is detail")
+        })
+        
+
     }
     
     func getUserLocation() async -> CLLocationCoordinate2D? {
@@ -155,13 +189,11 @@ struct DiscoverView: View {
         }
     }
     
-    func getDirections(to destination: CLLocationCoordinate2D) {
+    func getDirections() {
         Task {
-            guard let userLocation = await getUserLocation() else { return }
-            
             let waypoints: [CLLocationCoordinate2D] = [
-                userLocation,
-                CLLocationCoordinate2D(latitude: -6.270166, longitude: 106.821375),
+                .bbb,
+                .ccc,
                 .istiqlal
             ]
             
@@ -187,6 +219,24 @@ struct DiscoverView: View {
                 
         }
     }
+    
+    func getWalkingDirections(to destination: CLLocationCoordinate2D) {
+        Task {
+            guard let userLocation = await getUserLocation() else { return }
+            
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: .init(coordinate: userLocation))
+            request.destination = MKMapItem(placemark: .init(coordinate: destination))
+            request.transportType = .walking
+
+            do {
+                let directions = try await MKDirections(request: request).calculate()
+                route = directions.routes.first
+            } catch {
+                print("Show error")
+            }
+        }
+    }
 }
 
 #Preview {
@@ -196,5 +246,5 @@ struct DiscoverView: View {
 extension CLLocationCoordinate2D {
     static let istiqlal = CLLocationCoordinate2D(latitude: -6.170166, longitude: 106.831375)
     static let bbb = CLLocationCoordinate2D(latitude: -6.270166, longitude: 106.821375)
-
+    static let ccc = CLLocationCoordinate2D(latitude: -6.20166, longitude: 106.825375)
 }
