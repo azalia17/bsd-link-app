0a72297d5f29cebb1d1d1a7a9be4441560809752
Brayen <117528933+Scordatura92@users.noreply.github.com>
2025-03-29 17:34:09 +0700

    feat: Data structure of Route, Schedule, and BusStop added. Photos of the first three stops of route 1 added for testing

---
+++ BSDLink/Models/Route.swift
@@ -0,0 +1,45 @@
+
+import Foundation
+import MapKit
+
+struct Route: Identifiable{
+    let id = UUID()
+    let name: String
+    let busStops: [BusStop]
+    
+}
+
+var sampleRoutes: [Route] = [
+    Route(name: "Route 1", busStops: [
+        BusStop(name: "Intermoda",
+                coordinates: .intermoda,
+                bigHalte: true,
+                schedule: [
+                    
+                ],
+                images: ["Intermoda_1", "Intermoda_2"],
+                isBigHalte: true,
+                routes: <#T##[Route]#>),
+        
+        BusStop(name: "Cosmo",
+                coordinates: .cosmo,
+                bigHalte: true,
+                schedule: [
+                    
+                ],
+                images: ["Cosmo_1"],
+                isBigHalte: true,
+                routes: <#T##[Route]#>)
+        
+        BusStop(name: "Verdant View",
+                coordinates: .verdantView,
+                bigHalte: true,
+                schedule: [
+                    
+                ],
+                images: ["Verdant View_1"],
+                isBigHalte: true,
+                routes: <#T##[Route]#>)
+    ])
+    
+]
