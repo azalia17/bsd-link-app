be0343ae5b4e1e9c86ef2eafa1860b8dea0c498c
azalia17 <azaliaaps17@gmail.com>
2025-04-01 23:53:22 +0700

    feat: imagestack component for halte, refactor searchCard to white and add search function to reverse button

---
+++ BSDLink/Views/Components/ImageStack.swift
@@ -0,0 +1,18 @@
+//
+//  ImageStack.swift
+//  BSDLink
+//
+//  Created by Azalia Amanda on 01/04/25.
+//
+
+import SwiftUI
+
+struct ImageStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageStack()
+}
